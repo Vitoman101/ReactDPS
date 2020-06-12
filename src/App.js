c5270247859a6f9935b97e6e10abf66d375d657e
viktor.lekic.2403 <viktor.lekic95@gmail.com>
2020-06-12 10:12:13 +0200

    homepage

--- src/App.js
+++ src/App.js
@@ -1,32 +1,77 @@
-import React from 'react';
-import './App.css';
-import Nav from './Nav';
-import About from './About';
-import Example from './Example';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import React from "react";
+import "./App.css";
+import Nav from "./Nav";
+import About from "./About";
+import Example from "./Example";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+import { FaGraduationCap } from "react-icons/fa";
+
+var settings = {
+  dots: false,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+};
 
 function App() {
   return (
     <Router>
       <div className="App">
-      <Nav />
-      <Switch>
-        <Route path='/' exact component={Home} />
-        <Route path='/about' component={About}/>
-        <Route path='/example' component={Example}/>
-      </Switch>
-    </div>
+        <Nav />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/example" component={Example} />
+        </Switch>
+      </div>
     </Router>
-    
   );
 }
 
 const Home = () => (
   <div>
-    <h1>
-      Home Page
-    </h1>
+    <h3>Welcome to your exploration</h3>
+    <div className="darkerDiv">
+      <h3 className="darkTxt">Expenice the daily life of what you interest</h3>
+      <FaGraduationCap size={90}></FaGraduationCap>
+      <button className="darkbtn">LET'S GET STARTED</button>
+    </div>
+
+    <h2>Activites for your expenice</h2>
+    <Slider {...settings} className='slajder'>
+      <div className="container">
+        <div className="item">
+          <FaGraduationCap />
+        </div>
+        <div className="item">
+          <p>Some dummy text</p>
+        </div>
+      </div>
+      <div>
+        <h3>2</h3>
+      </div>
+      <div>
+        <h3>3</h3>
+      </div>
+      <div>
+        <h3>4</h3>
+      </div>
+      <div>
+        <h3>5</h3>
+      </div>
+      <div>
+        <h3>6</h3>
+      </div>
+    </Slider>
   </div>
-)
+
+  
+  
+);
 
 export default App;
--- src/Example.js
+++ src/Example.js
@@ -1,20 +1,15 @@
 import React from "react";
 import "./App.css";
 
-
-
 function Example() {
   return (
-      <div className='parent'>
-<div className='welcome'>
-      <h1>Welcome</h1>
-      <p className="lead">Let's find you the best study fields for you.</p>
-      <button className='mbtn'>
-          LET'S GET STARTED
-      </button>
-    </div>
+    <div className="parent">
+      <div className="welcome">
+        <h1>Welcome</h1>
+        <p className="lead">Let's find you the best study fields for you.</p>
+        <button className="mbtn">LET'S GET STARTED</button>
       </div>
-    
+    </div>
   );
 }
 
